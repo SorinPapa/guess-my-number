2d2453a77e199cedd18f93b09cadf06f45411b5c
SorinPapa <sorin.papa@ymail.com>
2022-12-29 12:00:50 +0200

    Guess My Number !

---
+++ script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  // when there is no input
+  if (!guess) {
+    displayMessage('⛔ No number!');
+
+    // when player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Corect Number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    displayNumber(secretNumber);
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  }
+
+  // refactoring
+  // when guess is wrong
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+      score--;
+      displayScore(score);
+    } else {
+      displayMessage('You lost the game');
+      displayScore(0);
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('body').style.backgroundColor = '#222';
+  displayMessage('Start guessing ...');
+  displayScore(score);
+  displayNumber('?');
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+});
